e9f26f4d9d751acc120aaf1887148cbd2df71f23
Keser <jbaptiste.jaillet@gmail.com>
2014-08-03 17:29:51 +0200

    some change

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3.4
 # coding=utf-8
+
 import pygame
 from pygame.locals import *
 from modele import Grille
+import sys
 
 demineur = Grille(10, 10)
 """demineur.displayGrid()
 demineur.fillGrid('easy')
 demineur.playAParty()"""
-POSX = 250
-POSY = 200
-SQUARE_SIZE = 16
+POSX = 150
+POSY = 100
+SQUARE_SIZE = 30
+
 pygame.init()
 
 main_frame = pygame.display.set_mode((640, 640), RESIZABLE)
+
 background = pygame.image.load('img/grey-background.jpg').convert()
 main_frame.blit(background, (0, 0))
+
 backgrd_x = demineur.rows * SQUARE_SIZE
 backgrd_y = demineur.column * SQUARE_SIZE
-backgrd_rect = Rect(POSX, POSY, backgrd_x, backgrd_y)
-#test = Rect(SQUARE_SIZE - 1, SQUARE_SIZE - 1, POSX, POSY)
-pygame.draw.rect(main_frame, Color("white"), backgrd_rect, 0)
-#pygame.draw.rect(main_frame, Color("black"), test, 1)
-rect_x = []
 
+backgrd_rect = Rect(POSX, POSY, backgrd_x, backgrd_y)
+pygame.draw.rect(main_frame, Color("white"), backgrd_rect, 0)
+
+font = pygame.font.SysFont('Arial', 10)
+
+rect_x = []
+y = 0
 i = POSX
 while i < POSX + backgrd_x:
     j = POSY
@@ -36,10 +43,18 @@ while i < POSX + backgrd_x:
 
 for line_x in rect_x:
     for line_y in line_x:
+        main_frame.blit(font.render(str(y), True, (0, 0, 0)), line_y.center)
+        y += 1
         pygame.draw.rect(main_frame, Color("black"), line_y, 1)
 
 pygame.display.flip()
 
-continu = 1
+continu = True
 while continu:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = False
+        """if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:"""
+
     continue
